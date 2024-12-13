bc1d9df8ce258b9225e1a213424b99ff861e89c5
kairos4213 <jamesdkairos@gmail.com>
2024-12-12 20:42:16 -0600

    Set up starter server and user migration

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: missing or misconfigured .env: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
