7b8a2eaeea7267b9c4e5fbe1ab46a09a3eded3b4
kairos4213 <jamesdkairos@gmail.com>
2025-03-10 20:57:39 -0500

    feat: add logout button

--- app/app.js
+++ app/app.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
 
   if (token) {
     document.getElementById("auth-section").style.display = "none";
     document.getElementById("user-home").style.display = "block";
+    document.getElementById("logout-button").style.display = "block";
     await getWorkouts();
   } else {
     document.getElementById("auth-section").style.display = "block";
     document.getElementById("user-home").style.display = "none";
+    document.getElementById("logout-button").style.display = "none";
   }
 });
 
-document.getElementById("login-form").addEventListener("submit", async (event) => {
-  event.preventDefault();
-  await login();
-});
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    await login();
+  });
+
+document
+  .getElementById("logout-button")
+  .addEventListener("onclick", async () => {
+    logout();
+  });
 
 async function login() {
   const email = document.getElementById("email").value;
@@ -26,17 +36,17 @@ async function login() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password}),
+      body: JSON.stringify({ email, password }),
     });
     const data = await res.json();
     if (!res.ok) {
       throw new Error(`Failed to login: ${data.error}`);
     }
-    
-    console.log(data);
+
     if (data.access_token) {
-      localStorage.setItem("token", data.access_token);
+      sessionStorage.setItem("token", data.access_token);
       document.getElementById("auth-section").style.display = "none";
+      document.getElementById("logout-button").style.display = "block";
       document.getElementById("user-home").style.display = "block";
       await getWorkouts();
     } else {
@@ -45,4 +55,11 @@ async function login() {
   } catch (error) {
     alert(`Error: ${error.message}`);
   }
+}
+
+function logout() {
+  sessionStorage.removeItem("token");
+  document.getElementById("auth-section").style.display = "block";
+  document.getElementById("user-home").style.display = "none";
+  document.getElementById("logout-button").style.display = "none";
 }
