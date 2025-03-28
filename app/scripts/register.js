63584584d3e1554a2c683f2d2897b564014b3cb1
kairos4213 <jamesdkairos@gmail.com>
2025-03-28 13:23:19 -0500

    feat(front-end): add register functionality

--- app/scripts/register.js
+++ app/scripts/register.js
@@ -1,21 +1,25 @@
 document
-  .getElementById("login-form")
+  .getElementById("register-form")
   .addEventListener("submit", async (event) => {
     event.preventDefault();
     login();
   });
 
 async function login() {
+  const first_name = document.getElementById("first_name").value;
+  const last_name = document.getElementById("last_name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    const res = await fetch("/api/v1/login", {
+    const res = await fetch("/api/v1/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        first_name,
+        last_name,
         email,
         password,
       }),
@@ -23,14 +27,14 @@ async function login() {
 
     const data = await res.json();
     if (!res.ok) {
-      throw new Error(`Failed to login: ${data.error}`);
+      throw new Error(`Failed to register: ${data.error}`);
     }
 
     if (data.access_token) {
       sessionStorage.setItem("token", data.access_token);
       window.location.href = "../pages/home.html";
     } else {
-      alert("Login failed, please check credentials");
+      alert("Failed to register new user. Please try again.");
     }
     return data;
   } catch (error) {
