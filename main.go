a03de01c78cbc8f63d501eb871eee531387452c8
James Kairos <jamesdkairos@gmail.com>
2026-02-13 09:47:51 -0600

    feat: add db connection pool config

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/kairos4213/fithub/internal/config"
@@ -43,6 +44,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(30 * time.Minute)
+	db.SetConnMaxIdleTime(5 * time.Minute)
 
 	dbQueries := database.New(db)
 	logger := slog.Default()
